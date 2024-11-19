511703798e57d5e68ff72b70afadbbf1307cdb53
Aiman Zafar <aniqaz456@gmail.com>
2024-11-19 15:47:20 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "google" {
 }
 
 resource "google_cloudfunctions_function" "visitor_counter" {
-  name        = "new-conuter-02"
+  name        = "new-conuter-02" 
   runtime     = "python311"
-  entry_point = "visitor_counter"
+  entry_point = "visitor_counter"  
   source_archive_bucket = "aiman-devops.site"
   source_archive_object = "cloud-resume-terraform.zip"
   trigger_http = true
